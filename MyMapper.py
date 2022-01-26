eb99e604eb70c8e000328e8be5f309caade38333
rohit.chandupatla@gmail.com <rohit.chandupatla@gmail.com>
2022-01-26 14:55:05 -0600

    Added output data

--- MyMapper.py
+++ MyMapper.py
@@ -6,11 +6,11 @@
 f = open("purchases.txt","r")  # open file, read-only
 o = open("MyOutputData.txt", "w") # open file, write
 for line in f:  
-    rowData = line.strip().split("    ") # DT: List of Lists
-    print (rowData )
-    print (len(rowData ))
-    if len(rowData) == 6:
-        date, time, location, dept, amount, payType = rowData
+    rowList = line.strip().split("    ") # DT: List of Lists
+    print (rowList )
+    print (len(rowList ))
+    if len(rowList) == 6:
+        date, time, location, dept, amount, payType = rowList
         #assign names
         print ("{0}\t{1}".format(location, amount))
         o.write("{0}\t{1}\n".format(location, amount))
